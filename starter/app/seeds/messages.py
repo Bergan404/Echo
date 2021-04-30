907c11e7aea5f9d4e01fa4d9066688d843db1faa
mike <mikemihalchik@gmail.com>
2021-04-30 10:08:41 -0400

    seeds done

--- starter/app/seeds/messages.py
+++ starter/app/seeds/messages.py
@@ -1,22 +1,33 @@
 from app.models import db, Message
 from datetime import datetime
-
+from faker import Faker
+import random
+fake = Faker()
 
 
 def seed_messages():
-    demo_message = Message(messages='The Demo message', reply='', user_id=1, channel_id=1, created_at=datetime.now())
-    demo_message2 = Message(messages='The Demo message2', reply='', user_id=2, channel_id=1, created_at=datetime.now())
-    demo_message3 = Message(messages='The Demo message3', reply='', user_id=2, channel_id=1, created_at=datetime.now())
-    demo_message4 = Message(messages='The Demo message4', reply='', user_id=1, channel_id=1, created_at=datetime.now())
-    demo_message5 = Message(messages='The Demo message5', reply='', user_id=1, channel_id=1, created_at=datetime.now())
-    demo_message6 = Message(messages='The Demo message6', reply='', user_id=2, channel_id=2, created_at=datetime.now())
+    def auto_seed(count, user_count, channel_count):
+        for i in range(count):
+            messages = fake.paragraph(nb_sentences=random.randint(1,5))
+            user_id = random.randint(1, user_count)
+            channel_id = random.randint(1, channel_count)
+            created_at = datetime.now()
+            seed_message = Message(messages= messages, user_id=user_id, channel_id=channel_id, created_at=created_at)
+            db.session.add(seed_message)
+    auto_seed(250, 50, 50)
+    # demo_message = Message(messages='The Demo message', reply='', user_id=1, channel_id=1, created_at=datetime.now())
+    # demo_message2 = Message(messages='The Demo message2', reply='', user_id=2, channel_id=1, created_at=datetime.now())
+    # demo_message3 = Message(messages='The Demo message3', reply='', user_id=2, channel_id=1, created_at=datetime.now())
+    # demo_message4 = Message(messages='The Demo message4', reply='', user_id=1, channel_id=1, created_at=datetime.now())
+    # demo_message5 = Message(messages='The Demo message5', reply='', user_id=1, channel_id=1, created_at=datetime.now())
+    # demo_message6 = Message(messages='The Demo message6', reply='', user_id=2, channel_id=2, created_at=datetime.now())
 
-    db.session.add(demo_message)
-    db.session.add(demo_message2)
-    db.session.add(demo_message3)
-    db.session.add(demo_message4)
-    db.session.add(demo_message5)
-    db.session.add(demo_message6)
+    # db.session.add(demo_message)
+    # db.session.add(demo_message2)
+    # db.session.add(demo_message3)
+    # db.session.add(demo_message4)
+    # db.session.add(demo_message5)
+    # db.session.add(demo_message6)
 
     db.session.commit()
 
--- starter/app/seeds/private_messages.py
+++ starter/app/seeds/private_messages.py
@@ -1,22 +1,36 @@
 from app.models import db, PrivateMessage
 from datetime import datetime
-
+from faker import Faker
+import random
+fake = Faker()
 
 
 def seed_private_messages():
-    demo_privateMessage1 = PrivateMessage(messages='The Demo privateMessage1', sender_id=1, reciever_id=2, created_at=datetime.now())
-    demo_privateMessage2 = PrivateMessage(messages='The Demo privateMessage2', sender_id=2, reciever_id=1, created_at=datetime.now())
-    demo_privateMessage3 = PrivateMessage(messages='The Demo privateMessage3', sender_id=2, reciever_id=1, created_at=datetime.now())
-    demo_privateMessage4 = PrivateMessage(messages='The Demo privateMessage4', sender_id=1, reciever_id=2, created_at=datetime.now())
-    demo_privateMessage5 = PrivateMessage(messages='The Demo privateMessage5', sender_id=1, reciever_id=2, created_at=datetime.now())
-    demo_privateMessage6 = PrivateMessage(messages='The Demo privateMessage6', sender_id=2, reciever_id=1, created_at=datetime.now())
+    def auto_seed(count, user_count):
+        for i in range(count):
+            messages = fake.paragraph(nb_sentences=random.randint(1,5))
+            sender_id = random.randint(1, user_count)
+            reciever_id = random.randint(1, user_count)
+            created_at = datetime.now()
+            while sender_id == reciever_id:
+                reciever_id = random.randint(1, user_count)
+            seed_private_message = PrivateMessage(messages=messages, sender_id=sender_id, reciever_id=reciever_id, created_at=created_at)
+            db.session.add(seed_private_message)
+    auto_seed(250, 50)
 
-    db.session.add(demo_privateMessage1)
-    db.session.add(demo_privateMessage2)
-    db.session.add(demo_privateMessage3)
-    db.session.add(demo_privateMessage4)
-    db.session.add(demo_privateMessage5)
-    db.session.add(demo_privateMessage6)
+    # demo_privateMessage1 = PrivateMessage(messages='The Demo privateMessage1', sender_id=1, reciever_id=2, created_at=datetime.now())
+    # demo_privateMessage2 = PrivateMessage(messages='The Demo privateMessage2', sender_id=2, reciever_id=1, created_at=datetime.now())
+    # demo_privateMessage3 = PrivateMessage(messages='The Demo privateMessage3', sender_id=2, reciever_id=1, created_at=datetime.now())
+    # demo_privateMessage4 = PrivateMessage(messages='The Demo privateMessage4', sender_id=1, reciever_id=2, created_at=datetime.now())
+    # demo_privateMessage5 = PrivateMessage(messages='The Demo privateMessage5', sender_id=1, reciever_id=2, created_at=datetime.now())
+    # demo_privateMessage6 = PrivateMessage(messages='The Demo privateMessage6', sender_id=2, reciever_id=1, created_at=datetime.now())
+
+    # db.session.add(demo_privateMessage1)
+    # db.session.add(demo_privateMessage2)
+    # db.session.add(demo_privateMessage3)
+    # db.session.add(demo_privateMessage4)
+    # db.session.add(demo_privateMessage5)
+    # db.session.add(demo_privateMessage6)
 
     db.session.commit()
 
